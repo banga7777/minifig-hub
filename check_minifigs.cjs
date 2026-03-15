de3c0bbd1ace59f5edeef549de35e7d2e0a09497
banga7777 <banga7777@naver.com>
2026-03-15 22:00:50 +0900

    Delete check_minifigs.cjs

--- check_minifigs.cjs
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('pages/MinifigDetail.tsx', 'utf8');
-const regex = /minifig\.item_no === '([^']+)'/g;
-const matches = [...content.matchAll(regex)].map(m => m[1]);
-console.log(matches.join(', '));
