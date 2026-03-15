d763dda2c7e2da677070fc2a9e9e2cb3aab2cfeb
banga7777 <banga7777@naver.com>
2026-03-15 21:59:49 +0900

    Delete src directory

--- src/services/characterClassifier.ts
+++
@@ -1,3 +0,0 @@
-export const CHARACTER_FAMILY = 'star-wars-family';
-
-export type StarWarsFamily = 'luke-skywalker' | 'darth-vader' | 'han-solo' | 'princess-leia' | 'obi-wan-kenobi' | 'yoda' | 'boba-fett' | 'din-djarin';
